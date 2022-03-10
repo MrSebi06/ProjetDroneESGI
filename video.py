6070170436ff80f3b3b61efc0908b516f2da38dc
Sebi <49305133+MrSebi06@users.noreply.github.com>
2022-03-10 14:57:43 +0100

    Update video.py

--- video.py
+++ video.py
@@ -1,40 +1,23 @@
-import cv2
-from djitellopy import Tello
-
-t = Tello()
-
-t.connect()
-
-t.streamoff()
-t.streamon()
-
-telloVideo = cv2.VideoCapture("udp://@0.0.0.0:11111")
-
-
-# wait for frame
-ret = False
-# scale down
-scale = 0.5
-
-while(True):
-    # Capture frame-by-framestreamon
-    ret, frame = telloVideo.read()
-    if(ret):
-    # Our operations on the frame come here
-        height , width , layers =  frame.shape
-        new_h=int(height/scale)
-        new_w=int(width/scale)
-        resize = cv2.resize(frame, (new_w, new_h)) # <- resize for improved performance
-        # Display the resulting frame
-        cv2.imshow('Tello',resize)
-        
-    if cv2.waitKey(1) & 0xFF == ord('s'):
-        cv2.imwrite("test.jpg",resize) # writes image test.bmp to disk
-        print("Take Picture")
+t.streamoff() 
+t.streamon()    
     
+telloVideo = cv2.VideoCapture("udp://@0.0.0.0:11111")
+    
+ret = False
+    
+while(True):
+ret, frame = telloVideo.read() 
+if(ret):                                    
+    cv2.imshow('Tello',frame)
+            
+    if cv2.waitKey(1) & 0xFF == ord('s'):
+        cv2.imwrite("test.jpg",frame)
+        print("Capture enregistrée !")
+        
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
-# When everything done, release the capture
+    
+# Quand tout est terminé, on termine et on ferme tout
 telloVideo.release()
 cv2.destroyAllWindows()
+return 0
