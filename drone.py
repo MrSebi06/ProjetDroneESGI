028899ef0054f7149f6a82f11574ec59b899b382
Sebi <49305133+MrSebi06@users.noreply.github.com>
2022-03-10 15:21:30 +0100

    Create drone.py

---
+++ drone.py
@@ -0,0 +1,25 @@
+import cv2
+from djitellopy import Tello
+
+class Drone:
+    def __init__(self):
+        self.t = Tello()
+        self.t.connect()
+        
+    def video(self):
+        self.telloVideo = cv2.VideoCapture("udp://@0.0.0.0:11111")   # On ouvre le fichier vidéo envoyé par la caméra IP du drone
+        self.ret = False  
+        
+        while (True):
+            self.ret, self.frame = self.telloVideo.read()
+            
+            if (self.frameret):
+                cv2.imshow('Tello', self.frame)
+                
+            if cv2.waitkey(1) & 0xFF == ord('q'):
+                break
+            
+        self.telloVideo.release()
+        cv2.destroyAllWindows()
+        
+        return 0
